3685e4cf14f1e8b5a61fdfffd510d8dfef3e0a32
Tero Tasanen <tero@tasanen.fi>
2014-10-14 19:51:00 +0300

    Output JSON in human readable format

--- app/interface.rb
+++ app/interface.rb
@@ -14,6 +14,8 @@ module LepoMongo
     content_type :json, 'application/json'
     content_type :xml, 'text/xml'
 
+    formatter :json, ->(obj, env) { JSON.pretty_generate(obj)  }
+
     helpers LepoMongo::Helpers
 
     mount LepoMongo::Documents
