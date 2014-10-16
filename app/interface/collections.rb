dd158a8f5e682703a4f4fd16d386181ed077333a
Tero Tasanen <tero@tasanen.fi>
2014-10-16 20:07:57 +0300

    Create, Delete and Rename collections

--- app/interface/collections.rb
+++ app/interface/collections.rb
@@ -14,17 +14,49 @@ module LepoMongo
     # POST /db
     # Creates a new collection
     post '/:db' do
+      unless database_exists?(params[:db])
+        error!("Could not find database #{params[:db]}", 404)
+      end
+
+      if collection_exists?(params[:db], params[:collection])
+        error!("Collection #{params[:collection]} already exists in #{params[:db]}", 406)
+      end
+
+      @db.create_collection(params[:collection])
+
+      response(201, {created: params[:collection], ok: 1})
     end
 
     # DELETE /db/collection
     # Deletes given collection
     delete '/:db/:collection' do
+      unless database_exists?(params[:db])
+        error!("Could not find database #{params[:db]}", 404)
+      end
+
+      unless collection_exists?(params[:db], params[:collection])
+        error!("Collection #{params[:collection]} does not exist in #{params[:db]}", 406)
+      end
+
+      @db.drop_collection(params[:collection])
+
+      response(200, {dropped: params[:collection], ok: 1})
     end
 
     # PATCH /db/collection
     # Updates collection (Rename)
     patch '/:db/:collection' do
-      #db.rename_collection
+      unless database_exists?(params[:db])
+        error!("Could not find database #{params[:db]}", 404)
+      end
+
+      unless collection_exists?(params[:db], params[:collection])
+        error!("Collection #{params[:collection]} does not exist in #{params[:db]}", 404)
+      end
+
+      @db.rename_collection(params[:collection], params[:new_collection_name])
+
+      response(200, {renamed_from: params[:collection], renamed_to: params[:new_collection_name], ok: 1})
     end
 
   end
